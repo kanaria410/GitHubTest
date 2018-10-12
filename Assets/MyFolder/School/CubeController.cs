28d1497a664869fad73fe615e337ca318c0beaab
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-10-12 09:52:53 +0900

    横移動機能を追加

--- Assets/MyFolder/School/CubeController.cs
+++ Assets/MyFolder/School/CubeController.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeController : MonoBehaviour {
+public class CubeController : MonoBehaviour
+{
+    [SerializeField, Header("スピード")]
+    Vector3 speed;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if (Input.GetAxis("Horizontal") != 0) 
+        transform.position += Input.GetAxis("Horizontal") * speed * Time.deltaTime;	
 	}
 }
