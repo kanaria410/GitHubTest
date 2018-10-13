599e2cf6a9fc4dbf1864ab87a11a14a41056c612
Koyata Sakurai <nkc.koyata.sakurai@gmail.com>
2018-10-13 16:36:15 +0900

    Test2回目

---
+++ Assets/Sakurai/TestScript2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("皆BO4買ってるけど、あれ買うくらいならコードヴェインとゴッドイーター3買うわ");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
